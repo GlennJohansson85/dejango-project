3101e9ef0dfef687c1b605a6b94af814d0d59230
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-01-17 17:01:01 +0100

    Add settings.py

---
+++ settings.py
@@ -0,0 +1,41 @@
+ # dejango-project/settings.py
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'hello_world',
+]
+
+MIDDLEWARE = [
+    # Add your middleware here
+]
+
+ROOT_URLCONF = 'dejango-project.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'dejango-project.wsgi.application'
+
+# ... other settings ...
+
+
